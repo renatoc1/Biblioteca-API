6e1a9a3563b28512e18e2cff32a2e2b71e36b9f1
= <renprata@hotmail.com>
2019-04-17 13:01:33 -0300

    Parte 3

--- src/main/java/com/renato/biblioteca/api/BibliotecaRenatoApiApplication.java
+++ src/main/java/com/renato/biblioteca/api/BibliotecaRenatoApiApplication.java
@@ -2,8 +2,12 @@ package com.renato.biblioteca.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.renato.biblioteca.api.config.property.BibliotecaProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(BibliotecaProperty.class)
 public class BibliotecaRenatoApiApplication {
 
 	public static void main(String[] args) {
